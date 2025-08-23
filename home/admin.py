dbded7a3a1570f915754131901698b07bab00a64
kimazatot <kimazatot@gmail.com>
2025-08-23 14:47:23 +0600

    fix: merge and fixes in translation.py

--- home/admin.py
+++ home/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
+from modeltranslation.admin import TranslationAdmin, TranslationTabularInline
+from _common.mixins import TranslatorMediaMixin
 from .models import Banner, AboutMovement, BrandMaterial
 
 @admin.register(Banner)
-class BannerAdmin(admin.ModelAdmin):
+class BannerAdmin(TranslatorMediaMixin, TranslationAdmin):
     list_display = ('description', 'cta_text', 'cta_link')
 
 @admin.register(AboutMovement)
-class AboutMovementAdmin(admin.ModelAdmin):
+class AboutMovementAdmin(TranslatorMediaMixin, TranslationAdmin):
     list_display = ('description',)
 
 # @admin.register(News)
@@ -14,5 +16,5 @@ class AboutMovementAdmin(admin.ModelAdmin):
 #     list_display = ('title', 'description', 'date')
 
 @admin.register(BrandMaterial)
-class BrandMaterialAdmin(admin.ModelAdmin):
+class BrandMaterialAdmin(TranslatorMediaMixin, TranslationAdmin):
     list_display = ('title', 'file')
