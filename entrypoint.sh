7bad24a4bbe5f9c9a1563385450f21584c1319aa
kimazatot <kimazatot@gmail.com>
2025-08-30 01:57:46 +0600

    fix: fixes in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,6 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(is_superuser=True).exists():
     User.objects.create_superuser(
-        username='admin',
         email='admin@example.com',
         password='1'
     )
