d195ad98a232d3087d3c4fb930105a868b6a4ad1
kimazatot <kimazatot@gmail.com>
2025-08-30 03:25:27 +0600

    fix: ебала я в рот эти ошибки, одни и те же файлы, одни и те же проблема, ПОЧЕМУ Я СТАЛА ПРОГРАММИСТОМ?

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -1,13 +1,11 @@
 import os
 from pathlib import Path
 from decouple import config
-from .jazzmin import JAZZMIN_SETTINGS, JAZZMIN_UI_TWEAKS
-
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
 
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-default-key')
+DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config(
     'ALLOWED_HOSTS',
     default='localhost,127.0.0.1',
@@ -15,29 +13,6 @@ ALLOWED_HOSTS = config(
 )
 
 AUTH_USER_MODEL = 'account.UserProfile'
-
-JAZZMIN_UI_TWEAKS = JAZZMIN_UI_TWEAKS
-JAZZMIN_SETTINGS = JAZZMIN_SETTINGS
-
-
-FRONTEND_URL = "http://38.180.136.75"
-
-
-
-CUSTOM_APPS = [
-    'content',
-    'about_direction',
-    'account',
-    'home',
-]
-
-THIRD_PARTY_APPS = [
-    'rest_framework',
-    'rest_framework.authtoken',
-    'drf_spectacular',
-    'django_rest_passwordreset'
-]
-
 
 INSTALLED_APPS = [
     'modeltranslation',
@@ -48,8 +23,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    *CUSTOM_APPS,
-    *THIRD_PARTY_APPS
+
+    'content',
+    'about_direction',
+    'account',
+    'home',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+    'drf_spectacular',
+    'django_rest_passwordreset',
 ]
 
 MIDDLEWARE = [
@@ -62,18 +45,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-SPECTACULAR_SETTINGS = {
-    'TITLE': 'Жаштар кыймылы',
-    'DESCRIPTION': 'Описание API',
-    'VERSION': '1.0.0',
-    'SERVE_INCLUDE_SCHEMA': True,
-}
-
-
-REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
-}
 
 ROOT_URLCONF = 'config.urls'
 
@@ -99,62 +70,45 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-
     }
 }
 
-
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'back_static'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    # BASE_DIR / 'locale_static',
-]
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
-MEDIA_URL = '/back_media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'back_media'
-
-
 
 LANGUAGES = (
     ('en', 'English'),
     ('ru', 'Russian'),
-    ('ky', 'Kyrgyzstan')
-
+    ('ky', 'Kyrgyz'),
 )
-
 MODELTRANSLATION_DEFAULT_LANGUAGE = 'ru'
 MODELTRANSLATION_LANGUAGES = ('ru', 'en', 'ky')
 
 TIME_ZONE = 'Asia/Bishkek'
-
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
-USE_L10N = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+}
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ryskulovaslan96@gmail.com'
-EMAIL_HOST_PASSWORD = 'uovkzbtiwbzgxquf'
