0f689f71f0a07bc073daff6f1042432b1a2cc223
Daniyar Marklenovich <daniyar01.m@gmail.com>
2025-09-18 17:22:53 +0600

    change db to postgres

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -104,8 +104,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': config('POSTGRES_HOST', 'localhost'),
+        'PORT': config('POSTGRES_PORT', '5432'),
     }
 }
 
