da94886e60e5cd7aae32b8a10cd2062b83c0c955
ba1ai.ql <joldoshbekovbaiel6@gmail.com>
2025-09-19 17:18:45 +0600

    + s3

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -120,8 +120,20 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'back_static'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'back_media'
+
+AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = config("AWS_S3_BUCKET_NAME")
+AWS_S3_ENDPOINT_URL = config("AWS_S3_ENDPOINT_URL")
+AWS_S3_REGION_NAME = config("AWS_S3_REGION_NAME")
+AWS_S3_CUSTOM_DOMAIN = config("AWS_S3_CUSTOM_DOMAIN")
+AWS_LOCATION = config("AWS_S3_MEDIA_UPLOAD_DIR", default="production")
+
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
+
+
 
 LANGUAGES = (
     ('en', 'English'),
