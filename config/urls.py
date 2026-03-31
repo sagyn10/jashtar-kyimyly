27cc11f284884b9f25fb9382373380ebe8e69b52
Aman <saynbaevaman7.09@gmail.com>
2026-03-31 14:44:44 +0600

    fix4

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView, 
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from account.views import create_magic_admin
 
 urlpatterns = [
